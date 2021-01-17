045064662c3e6aa1601c2053960543407324035b
kevinjycui <kevinjycui@protonmail.com>
2021-01-17 03:29:27 -0500

    Enable varying knife speeds

--- UbiGame/KnifeMovementComponent.cpp
+++ UbiGame/KnifeMovementComponent.cpp
@@ -1,9 +1,12 @@
 #include "KnifeMovementComponent.h"
 #include <SFML/Window/Keyboard.hpp>   //<-- Add the keyboard include in order to get keyboard inputs
-
+#include <algorithm>
 #include "GameEngine/GameEngineMain.h" //<-- Add this include to retrieve the delta time between frames
 
 using namespace Game;
+
+static float inputAmount = 300.0f;
+static float increment = 0.01f;
 
 void KnifeMovementComponent::Update()
 {
@@ -17,7 +20,13 @@ void KnifeMovementComponent::Update()
     sf::Vector2f position = GetEntity()->GetPos();
 
     //The amount of speed that we will apply when input is received
-    const float inputAmount = 300.0f;
+
+    inputAmount += increment;
+
+    if (inputAmount > 900.f)
+        increment = -0.01f;
+    else if (inputAmount < 300.f)
+        increment = 0.01f;
 
     displacement.x += inputAmount * dt * direction;
 
